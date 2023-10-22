06710e9f149d88ca2121bbd5f4989f470b39e3f0
Surya Prakash <suryaprakashx4@gmail.com>
2023-10-22 15:06:18 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
