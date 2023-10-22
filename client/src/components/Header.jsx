6b5594124a5cf2809e3a8a5ef9e830534a3b7fd0
Surya Prakash <suryaprakashx4@gmail.com>
2023-10-22 15:27:45 +0530

    Header Syntax done

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white mb-5">
+      <h1 className="text-3xl font-semibold">Job Portal</h1>
+    </header>
+  );
+};
+
+export default Header;
